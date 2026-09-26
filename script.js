e47a6b1031ac6930d746b32a6dc0c664eeeb4e75
Zentroworldconnect <nachiappankr5@gmail.com>
2026-09-26 13:11:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -209,20 +209,7 @@ if (sets.aboutDescription) {
    document.querySelectorAll('.nav-to-catalog').forEach(btn => {
     btn.addEventListener('click', (e) => {
         e.preventDefault();
-
-        const category = btn.getAttribute('data-category');
-
         showView('catalog');
-
-        if (category) {
-            const categoryButton = document.querySelector(
-                `.pill-btn[data-cat="${category}"]`
-            );
-
-            if (categoryButton) {
-                categoryButton.click();
-            }
-        }
     });
 });
 
